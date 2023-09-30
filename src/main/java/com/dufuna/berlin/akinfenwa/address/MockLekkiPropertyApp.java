708cc748c3a8cec46c5fcb3a48a539faa3587f7e
chris <akinfenwataiwo2019@gmail.com>
2023-09-30 22:37:11 +0100

    Feature/jcc_Sprint3

--- src/main/java/com/dufuna/berlin/akinfenwa/address/MockLekkiPropertyApp.java
+++ src/main/java/com/dufuna/berlin/akinfenwa/address/MockLekkiPropertyApp.java
@@ -1,7 +1,20 @@
 package com.dufuna.berlin.akinfenwa.address;
 
+import com.dufuna.berlin.akinfenwa.address.model.LekkiProperty;
+import com.dufuna.berlin.akinfenwa.address.service.LekkiPropertyService;
+import com.dufuna.berlin.akinfenwa.address.service.MockLekkiPropertyService;
+
 public class MockLekkiPropertyApp {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        LekkiProperty property= new LekkiProperty();
+        property.setPropertyId("12345");
+        property.setPropertyAddress("24 Ibadan");
+
+        LekkiPropertyService propertyService = new MockLekkiPropertyService();
+        propertyService.saveProperty(property);
+
+        LekkiProperty retrievedProperty = propertyService.getProperty();
+
+
     }
 }
